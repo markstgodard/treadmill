3ad036a7867084bb38b63ac3d95fed564a0c90f3
Mark St.Godard <markstgodard@gmail.com>
2016-03-23 21:33:43 -0500

    update

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
-echo "Starting NATS.."
+if [ -n "${GOROUTER_DIR+1}" ]; then
+  echo "Gorouter dir: ${GOROUTER_DIR}"
+else
+  echo "GOROUTER_DIR env varirable is not defined"
+  exit 1;
+fi
+
+echo "Installing stuff.."
+# go stuff
 go get github.com/apcera/gnatsd
+go get github.com/rakyll/boom
+# ruby stuff
+gem install nats
+
+echo "Starting NATS.."
 gnatsd &
 
 echo "Building sample app"
@@ -13,26 +26,35 @@ bin/apps.sh
 echo "Starting registrar.."
 bin/registrar.sh &
 
-echo "Restoring godeps.."
-godep restore ./...
+# echo "Restoring godeps.."
+# godep restore ./...
 
-echo "Building.."
-go build
+# echo "Change to gorouter dir"
+# cd $GOROUTER_DIR
 
-echo "Starting gorouter.."
-./gorouter -c ./config/gorouter.yml &
+# echo "Building gorouter.."
+# go install
 
-echo "\tsleeping for a few seconds.."
-sleep 25
+# echo "Starting gorouter.."
+# ./gorouter -c ./config/gorouter.yml &
 
-echo "Starting load test"
-echo -n "$i ," >> ~/out/loadtest.out
-boom -n 10-c 2 -x http://localhost:8081 http://demo.vcap.me  | grep -E "Requests/sec|responses" | tr -s "\n" "," | cut -f 2 -d ":" >> ~/out/loadtest.out
-echo ""
-echo "complete"
+# echo "\tsleeping for a few seconds.."
+# sleep 25
 
-echo "killing gorouter.."
-kill -9 `pgrep gorouter`
+# echo "Starting load test"
+# echo -n "$i ," >> ~/out/loadtest.out
+# boom -n 10-c 2 -x http://localhost:8081 http://demo.vcap.me  | grep -E "Requests/sec|responses" | tr -s "\n" "," | cut -f 2 -d ":" >> ~/out/loadtest.out
+# echo ""
+# echo "complete"
+
+# echo "killing gorouter.."
+# kill -9 `pgrep gorouter`
+
+echo "killing apps.."
+kill -9 `pgrep treadmill`
+
+echo "killing registrar.."
+kill -9 `pgrep registrar.sh`
 
 echo "killing gnatsd.."
 kill -9 `pgrep gnatsd`
