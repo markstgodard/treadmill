7c39aa19a441d55a6092f5950729909ebfbdeb0d
Mark St.Godard <markstgodard@gmail.com>
2016-03-23 21:52:39 -0500

    cleanup

--- bin/run.sh
+++ bin/run.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 if [ -n "${GOROUTER_DIR+1}" ]; then
-  echo "Gorouter dir: ${GOROUTER_DIR}"
+  echo "GOROUTER_DIR: ${GOROUTER_DIR}"
 else
   echo "GOROUTER_DIR env varirable is not defined"
   exit 1;
 fi
 
+APPDIR=$PWD
+
 echo "Installing stuff.."
 # go stuff
+go get -v github.com/tools/godep
 go get github.com/apcera/gnatsd
 go get github.com/rakyll/boom
 # ruby stuff
@@ -25,36 +28,38 @@ bin/apps.sh
 
 echo "Starting registrar.."
 bin/registrar.sh &
+echo $$
 
-# echo "Restoring godeps.."
-# godep restore ./...
+echo "Change to gorouter dir"
+cd $GOROUTER_DIR
+echo $GOPATH
 
-# echo "Change to gorouter dir"
-# cd $GOROUTER_DIR
+echo "Restoring godeps.."
+godep restore ./...
 
-# echo "Building gorouter.."
-# go install
+echo "Building gorouter.."
+go build
 
-# echo "Starting gorouter.."
-# ./gorouter -c ./config/gorouter.yml &
+echo "Starting gorouter.."
+./gorouter -c $APPDIR/config/gorouter.yml &
 
-# echo "\tsleeping for a few seconds.."
-# sleep 25
+echo "  sleeping for a few seconds to ensure app routes are registered.."
+sleep 25
 
-# echo "Starting load test"
-# echo -n "$i ," >> ~/out/loadtest.out
-# boom -n 10-c 2 -x http://localhost:8081 http://demo.vcap.me  | grep -E "Requests/sec|responses" | tr -s "\n" "," | cut -f 2 -d ":" >> ~/out/loadtest.out
-# echo ""
-# echo "complete"
+echo "Starting load test"
+echo -n "$i ," >> $APPDIR/loadtest.out
+boom -n 10 -c 2 -x http://localhost:8081 http://demo.vcap.me  | grep -E "Requests/sec|responses" | tr -s "\n" "," | cut -f 2 -d ":" >> $APPDIR/loadtest.out
+echo ""
+echo "complete"
 
-# echo "killing gorouter.."
-# kill -9 `pgrep gorouter`
+echo "killing gorouter.."
+kill -9 `pgrep gorouter`
 
 echo "killing apps.."
 kill -9 `pgrep treadmill`
 
 echo "killing registrar.."
-kill -9 `pgrep registrar.sh`
+kill -9 `ps ux | awk '/bin\/registrar.sh/ {print $2}'`
 
 echo "killing gnatsd.."
 kill -9 `pgrep gnatsd`
